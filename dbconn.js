df534fb2de0984100ed285b0b7c4a8f5b3422b1c
komarov <komkim@mail.ru>
2018-11-14 17:30:56 +0300

    add new db connect

--- dbconn.js
+++ dbconn.js
@@ -1,27 +1,20 @@
 var mysql = require('mysql');
-//var connection = mysql.createPool({
-//  host     : '46.147.193.8',
-//  port : '1915',
-//  user     : 'igor',
-//  password : 'qwerty12345',
-//  database : 'ukn',
-//  dateStrings: true
-//});
-//var connection = mysql.createPool({
-//  host     : 'localhost',
-//  port : '3306',
-//  user     : 'root',
-//  password : '',
-//  database : 'ukn_office',
-//  dateStrings: true
-//});
-//module.exports = connection;
+/*
 var connection = mysql.createPool({
   host     : '31.31.196.172',
   port : '3306',
   user     : 'u0579301_igor',
   password : 'qwerty12345',
   database : 'u0579301_ukn',
+  dateStrings: true
+});
+*/
+var connection = mysql.createPool({
+  host     : '213.183.51.110',
+  port : '3306',
+  user     : 'igor',
+  password : 'deDnmr1x',
+  database : 'ukn',
   dateStrings: true
 });
 module.exports = connection;
